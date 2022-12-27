bd92482ab77447f84a7456818961117cf61f0b7f
TheBoyWhoLivedd <98583453+TheBoyWhoLivedd@users.noreply.github.com>
2022-12-27 15:55:53 +0300

    added aggregation pipeline for amount requested

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,8 +2,8 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:3500",
-    "https://kotedx-3000.preview.csb.app",
-    // "https://technotes.onrender.com",
+    
+    "https://materialschedule.onrender.com",
 ];
 
 
