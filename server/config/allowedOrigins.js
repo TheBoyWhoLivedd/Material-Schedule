a939c36d74024687d83b18135eefac5ddf2578e7
TheBoyWhoLivedd <98583453+TheBoyWhoLivedd@users.noreply.github.com>
2023-05-03 19:15:54 +0300

    Changed hosting to namecheap server

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,20 +1,13 @@
 const allowedOrigins = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://localhost:3500",
-    "https://materialschedule.onrender.com",
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3500",
+  "https://materialschedule.onrender.com",
+  "https://server-materialschedule.hitajitech.site",
 
-    "https://hitajitech.site/server-deemedvat",
+  "https://hitajitech.site/server-deemedvat",
 
-    "https://materialschedule-api.onrender.com",
-
+  "https://materialschedule-api.onrender.com",
 ];
 
-
-
 module.exports = allowedOrigins;
-
-
-
-
-
